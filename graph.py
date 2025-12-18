249272350058d2f46ec9686519f31fcb4a5d9573
harshithaaramamoorthy-glitch <harshithaaramamoorthy@gmail.com>
2025-12-18 16:27:13 +0530

    18.12.25

---
+++ graph.py
@@ -0,0 +1,11 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+    def addvertex(self,v):
+        if v not in self.graph:
+            self.graph[v] = []
+
+
+        if __name__ == "__main__":
+         pass
+    
